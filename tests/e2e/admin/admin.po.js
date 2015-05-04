e4af1d264c3e254e9d355618f5427d5ce25e920e
jacketaj <blazekjiri1@gmail.com>
2015-05-04 19:40:09 +0200

    #3 implement tests for admin page

--- tests/e2e/admin/admin.po.js
+++ tests/e2e/admin/admin.po.js
@@ -9,5 +9,86 @@ var Admin = function () {
         element(by.id('password')).sendKeys(pw);
         element(by.id('logbtn')).click();
     };
+
+    this.getToAdmin = function () {
+        element(by.id('admin')).click();
+    };
+
+    // units
+    this.getToUnits = function () {
+        element(by.css('[ng-click="tab.setTab(3)"]')).click();
+    };
+
+    this.getUnits = function () {
+        return element.all(by.repeater('unit in units'));
+    };
+
+    this.createUnit = function (name, quota, admins) {
+        element(by.css('[mac-modal="orgunit"]')).click();
+        element(by.model('unit.name')).sendKeys(name);
+        element(by.model('unit.quota')).sendKeys(quota);
+        element(by.model('unit.admins')).sendKeys(admins);
+        element(by.css('[type="submit"]')).click();
+    };
+
+    this.editUnit = function (name, quota, admins) {
+        element(by.css('[ng-click="editUnit($index, unit)"]'));
+        var vname = element(by.model('unit.name'));
+        var vquota = element(by.model('unit.quota'));
+        var vadmins = element(by.model('unit.admins'));
+        vname.clear();
+        vname.sendKeys(name);
+        vquota.clear();
+        vquota.sendKeys(quota);
+        vadmins.clear();
+        vadmins.sendKeys(admins);
+        element(by.css('[type="submit"]')).click();
+    };
+
+    this.deleteUnit = function () {
+        element(by.css('[ng-click="deleteUnit($index)"]')).click();
+    };
+
+    // externists
+    this.getToExternists = function () {
+        element(by.css('[ng-click="tab.setTab(4)"]')).click();
+    };
+    this.getExternists = function () {
+        return element.all(by.repeater('external in externists'));
+    };
+
+    this.createExternist = function (name, email, quota, unit, onuser) {
+        element(by.css('[mac-modal="externist"]')).click();
+        element(by.model('external.name')).sendKeys(name);
+        element(by.model('external.email')).sendKeys(email);
+        element(by.model('external.quota')).sendKeys(quota);
+        element(by.model('external.unit')).sendKeys(unit);
+        element(by.model('external.onuser')).sendKeys(onuser);
+        element(by.css('[type="submit"]')).click();
+    };
+
+    this.editExternist = function (name, email, quota, unit, onuser) {
+        element(by.css('[ng-click="editUser($index, external)"]'));
+        var vname = element(by.model('external.username'));
+        var vemail = element(by.model('external.email'))
+        var vquota = element(by.model('external.quota'));
+        var vunit = element(by.model('external.unit'));
+        var vonuser = element(by.model('external.onuser'));
+        vname.clear();
+        vname.sendKeys(name);
+        vemail.clear();
+        vemail.sendKeys(email);
+        vquota.clear();
+        vquota.sendKeys(quota);
+        vunit.clear();
+        vunit.sendKeys(unit);
+        vonuser.clear();
+        vonuser.sendKeys(onuser);
+        element(by.css('[type="submit"]')).click();
+    };
+
+    this.deleteExternist = function () {
+        element(by.css('[ng-click="deleteUser($index)"]')).click();
+    };
 };
 module.exports = Admin;
--- tests/e2e/admin/admin.spec.js
+++ tests/e2e/admin/admin.spec.js
@@ -11,6 +11,61 @@ describe('integracni-portal - admin', function () {
         admin.login(usr, usr);
         expect(browser.getTitle()).toContain(usr);
     });
+
+    it('should get to admin page', function () {
+        admin.getToAdmin();
+        expect(browser.getCurrentUrl()).toContain('admin');
+    });
+
+    it('should load org units', function () {
+        expect(admin.getUnits().count() > 0);
+    });
+
+    it('should create orgunit', function () {
+        var count = admin.getUnits().count();
+        admin.createUnit('newUnit', 34, 'email@gege.com');
+        expect(admin.getUnits.count() === count + 1);
+    });
+
+    it('should edit orgunit', function () {
+        admin.editUnit('editted', 1, 'gege@gegw.gew');
+        expect(admin.getUnits().row(0).getText()).toContain('editted', 'gege@gegw.gew');
+    });
+
+    it('should delete orgunit', function () {
+        var count = admin.getUnits();
+        admin.deleteUnit();
+        expect(admin.getUnits().count() === count - 1);
+    });
+
+    // externists
+    it('should go to externists', function () {
+        admin.getToExternists();
+        expect(admin.getExternists().count() > 0);
+    });
+
+    it('should create externist', function () {
+        var count = admin.getExternists().count();
+        admin.createExternist('newUser', 'email@gege.com', 32, 'testunit', 'onuser@gre.hre');
+        expect(admin.getExternists.count() === count + 1);
+    });
+
+    it('should edit externist', function () {
+        admin.editExternist('edittedUser', 'email@gegewge.com', 52, 'testunit2', 'onuser2@gre.hre');
+        expect(admin.getExternists().row(0).getText()).toContain('edittedUser', 'email@gegewge.com');
+    });
+
+    it('should delete externist', function () {
+        var count = admin.getExternists();
+        admin.deleteExternist();
+        expect(admin.getExternists().count() === count - 1);
+    });
+
+    // back to units
+    it('should go to orgunits', function () {
+        admin.getToUnits();
+        expect(admin.getUnits().count() > 0);
+    });
 });
 
 
