b4533d7bc589059e66868eba03a523bf7955762e
Radek Ježdík <redhead@email.cz>
2015-08-11 20:57:01 +0200

    fixed configs

--- config/env_test.js
+++ config/env_test.js
@@ -1,8 +1,8 @@
 module.exports = function () {
 	var environment = {
 		rootContext: "integracni-portal-ui",
-		baseUrl: 'http://localhost:8080/integracni-portal/rest/v0.2',
-		oauthUrl: 'http://localhost:8080/integracni-portal'
+		baseUrl: 'http://147.32.80.219:8080/integracni-portal/rest/v0.2',
+		oauthUrl: 'http://147.32.80.219:8080/integracni-portal'
 	};
 	return environment;
 };
